b9d0dccb7b6fca0c539bd8bbd7280b40939c3c2e
Irina <116427830+11Rin@users.noreply.github.com>
2023-04-16 12:12:44 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -5,47 +5,48 @@
 
 template<typename T, int size>
 class TPQueue {
- private:
-   T* arr;
-   int head, tail, cnt;
- public:
-   TPQueue() : head(0), tail(0), cnt(0) {
-     arr = new T[size];
-   }
-   ~TPQueue() {
-     delete[] arr;
-   }
-   bool isEmpty() const {
-     return cnt == 0;
-   }
-   bool isFull() const {
-     return cnt == size;
-   }
-   void push(const T& value) {
-     if (isFull()) {
-       throw std::string("Full");
-     }
-     cnt++;
-     int curr = tail;
-     for (int i = head; i < tail; i++) {
-       if (arr[i].prior < value.prior) {
-         curr = i;
-	 break;
-       }
-     }
-     for (int i = tail; i > curr; i--) {
-       arr[i % size] = arr[(i - 1) % size];
-     }
-     arr[curr % size] = value;
-     tail += 1;
-   }
-   T& pop() {
-     if (isEmpty()) {
-       throw std::string("Empty");
-     }
-     cnt--;
-     return arr[head++ % size];
-   }
+  private:
+    T* arr;
+    int head, tail, cnt;
+   
+  public:
+    TPQueue() : head(0), tail(0), cnt(0) {
+      arr = new T[size];
+    }
+    ~TPQueue() {
+      delete[] arr;
+    }
+    bool isEmpty() const {
+      return cnt == 0;
+    }
+    bool isFull() const {
+      return cnt == size;
+    }
+    void push(const T& value) {
+      if (isFull()) {
+        throw std::string("Full");
+      }
+      cnt++;
+      int curr = tail;
+      for (int i = head; i < tail; i++) {
+        if (arr[i].prior < value.prior) {
+          curr = i;
+          break;
+        }
+      }
+      for (int i = tail; i > curr; i--) {
+        arr[i % size] = arr[(i - 1) % size];
+      }
+      arr[curr % size] = value;
+      tail += 1;
+    }
+    T& pop() {
+      if (isEmpty()) {
+        throw std::string("Empty");
+      }
+      cnt--;
+      return arr[head++ % size];
+    }
 };
 
 struct SYM {
